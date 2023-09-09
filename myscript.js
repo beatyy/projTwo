97a25ce75dba79ff993d479243df3594f7e438c5
Yang.Beaty <yang.beaty@caci.com>
2023-09-09 16:38:29 -0600

    create table in function

--- myscript.js
+++ myscript.js
@@ -11,7 +11,6 @@ async function getapi(url) {
     }
     show(data.results);
 }
-
 getapi(api_url);
 
 function hideloder() {
@@ -19,18 +18,70 @@ function hideloder() {
 }
 
 function show(data) {
-    let tab =
-    `<tr>
-    <th>Name</th>
-    <th>url</th>
-   </tr>`
-
-   for(let r in data) {
-        const obj = data[r];
-        tab += `<tr>
-        <td>${obj.name} </td>
-        <td><a href=\"${obj.url}\"</a>${obj.url}</td>        
-    </tr>`
-   }
-    document.getElementById("pokemon").innerHTML = tab;
+    let tableH = ["Name", "url"]
+    let table = document.getElementById("pokemon");
+    generateTableHead(table, tableH)
+    generateTable(table, data);
 }
+
+/*function displayBerries(moo) {
+    let table = document.getElementById("berries");
+    //let data = Object.keys(moo);
+    let tableH = ["Berries", "url"];
+    generateTableHead(table, tableH)
+    generateSecondTable(table, moo);
+}*/
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for(let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTable(table, data) {
+    for(let element of data) {
+        let row = table.insertRow();
+        for(key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+           
+            cell.appendChild(text)
+        }
+    }
+}
+/*
+function generateSecondTable(table, data) {
+    for(let element of data) {
+        let row = table.insertRow();
+        for(key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            if(key == 'url') {               
+                text.addEventListener('click', function(){
+                    getSecondApi(element[key]);
+                })
+            }
+            cell.appendChild(text)
+        }
+    }
+
+}
+
+async function getSecondApi(url) {
+    const response = await fetch(url)
+
+    var data = await response.json();
+    console.log(data);
+
+    if(response) {
+        hideloder();
+    }
+    displayBerries(data.results);
+}*/
+
+ 
